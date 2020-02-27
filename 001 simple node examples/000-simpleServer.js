70f2222ff48ebd79c9886d658701c993d8ee370d
danilads <vfhn3fghtkm>
2020-02-27 20:08:08 +0300

    updt

---
+++ 001 simple node examples/000-simpleServer.js
@@ -0,0 +1,7 @@
+var net=require('net');
+
+var server=net.createServer( function(socket) {
+  соnsоle.lоg('Соединение с '+socket.remoteAddress+':'+socket.remotePort);
+} ).listen(8080);
+
+console.log('listening on port 8080');
