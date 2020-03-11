dbeec8163ecc4127c819a56a0bdf3a2ae9a80c1a
danilads <vfhn3fghtkm>
2020-03-11 16:58:57 +0300

    upd

--- 003 package node/003 return file indexHtml script/packServer003.js
+++ 003 package node/003 return file indexHtml script/packServer003.js
@@ -5,8 +5,12 @@ const webserver = express();
 var path = require("path");
 const port = 8080;
 
+webserver.use(express.static('public'));//Открываем доступ к папке src, чтобы index.html мог достучаться
+
 webserver.get('/', (req, res) => {
 	console.log('Пришёл запрос от клиента');
+	
+	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
